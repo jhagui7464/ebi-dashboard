be51d38e71d03068a8bcf7e6f766adb0db952524
Jose Aguilar <jhagui7464@gmail.com>
2021-08-09 17:59:16 -0500

    navbar and data added

---
+++ src/components/SidebarData.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+import * as GiIcons from "react-icons/gi";
+
+export const SidebarData = [{
+    title: "Home",
+    path: '/',
+    icon: <AiIcons.AiFillHome />,
+    className: 'nav-text'
+},
+{
+    title: "Cold Storage",
+    path: '/coldStorage',
+    icon: <GiIcons.GiThermometerCold />,
+    className: 'nav-text'
+},
+{
+    title: "Traffic",
+    path: '/traffic',
+    icon: <FaIcons.FaTruckLoading />,
+    className: 'nav-text'
+},
+{
+    title: "Transport",
+    path: '/transport',
+    icon: <FaIcons.FaTruckMoving />,
+    className: 'nav-text'
+},
+{
+    title: "Logout",
+    path: '/h',
+    icon: <AiIcons.AiOutlineLogout />,
+    className: 'nav-text'
+},]
