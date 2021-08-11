c1b4b584ebaa0717de2ed0fc244a3231e915be3f
Jose Aguilar <jhagui7464@gmail.com>
2021-08-11 17:48:41 -0500

    dashboard update

---
+++ src/pages/coldStorage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function coldStorage() {
+    return (
+        <div className='coldStorage'>
+            <h1>Cold Storage</h1>    
+        </div>
+    )
+}
+
+export default coldStorage
+
