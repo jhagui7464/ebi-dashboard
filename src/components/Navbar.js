d5909fb18dec234ce5d4db9452972e1d469f111f
Jose Aguilar <jhagui7464@gmail.com>
2021-08-11 17:22:53 -0500

    dashboard update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,15 @@ import {Link} from "react-router-dom";
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
 import { SidebarData } from './SidebarData'
-
+import './Navbar.css'
+import {IconContext} from 'react-icons'
 function Navbar() {
     const[sidebar, setSidebar] = useState(false)
 
     const showSidebar = () => setSidebar(!sidebar)
   return (
     <>
+    <IconContext.Provider value={{ color: '#fff'}}>
       <div className="navbar">
         <Link to="#" className="menu-bars">
           <FaIcons.FaBars onClick={showSidebar}/>
@@ -17,7 +19,7 @@ function Navbar() {
       </div>
 
       <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>    
-      <ul className='nav-menu-items'>
+      <ul className='nav-menu-items' onClick={showSidebar}>
           <li className='navbar-toggle'>
               <Link to="#" className='menu-bars'>
                   <AiIcons.AiOutlineClose/>
@@ -37,6 +39,7 @@ function Navbar() {
           })}
       </ul>
       </nav>
+      </IconContext.Provider>
     </>
   );
 }
